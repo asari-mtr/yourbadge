679b336ef276dbd1299a9830e4a858dfe3b850d1
Asari Mitsuteru <asari.mtr@gmail.com>
2015-08-20 18:26:01 +0900

    関連付け

--- app/models/archivement.rb
+++ app/models/archivement.rb
@@ -1,2 +1,4 @@
 class Archivement < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :condition
 end
--- db/migrate/20150820075521_create_archivements.rb
+++ db/migrate/20150820075521_create_archivements.rb
@@ -1,6 +1,8 @@
 class CreateArchivements < ActiveRecord::Migration
   def change
     create_table :archivements do |t|
+      t.references :user, index: true
+      t.references :condition, index: true
       t.integer :number
       t.string :memo
 
